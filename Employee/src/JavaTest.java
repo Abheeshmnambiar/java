830ded0423d6e647e1ef7ada376786f29cc9411e
abheesh m <abheesh m@dell.Dlink>
2018-11-12 12:51:26 +0530

    second commit

--- Employee/src/JavaTest.java
+++ Employee/src/JavaTest.java
@@ -1,4 +1,21 @@
 
 public class JavaTest {
-
+	public static void main(String args[]){
+		 double a= 20;
+		 double b=80;
+		 
+		 double c;
+		 c=(a+b)*25;
+		 System.out.println("total =" + c);
+		 double d;
+		 d=c%40;
+		 
+		 System.out.println("reminder=" + d); 
+		 if(d<=20)
+		 {
+			  System.out.println("result was over limit");
+		 }
+		 
+	 }
+	
 }
