e607c6858b59907bb9c5e75175c2489f8c3eeba6
Abheesh <abheeshmnambiar@gmail.com>
2018-12-11 14:54:27 +0530

    commit

---
+++ Encapsulation/src/EmpTest.java
@@ -0,0 +1,14 @@
+
+public class EmpTest {
+
+	   public static void main(String args[]){
+	         EmployeeExample obj = new EmployeeExample();
+	         obj.setEmpName("arun");
+	         obj.setEmpAge(32);
+	         obj.setEmpSSN(112233);
+	         System.out.println("Employee Name: " + obj.getEmpName());
+	         System.out.println("Employee SSN: " + obj.getEmpSSN());
+	         System.out.println("Employee Age: " + obj.getEmpAge());
+	    } 
+	}
+
---
+++ Encapsulation/src/Test.java
@@ -0,0 +1,12 @@
+
+public class Test {
+	public static void main(String[] args){  
+	
+		Student s=new Student();  
+		
+		s.setName("vijay");  
+		  
+		System.out.println(s.getName());  
+		}  
+		}  
+
